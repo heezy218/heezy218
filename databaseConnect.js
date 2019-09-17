ed2134688bfd5fcd55d0056a9ba57defaeedb75b
heezy218 <heezy193@gmail.com>
2019-09-17 16:45:14 +0900

    오늘 끝!!

--- databaseConnect.js
+++ databaseConnect.js
@@ -13,7 +13,7 @@ connection.connect();
 
 connection.query('SELECT * FROM user', function (error, results, fields) {
     if (error) throw error;
-    console.log('The solution is: ', results);
+    console.log(results);
 });
 
 connection.end();
--- express.js
+++ express.js
@@ -12,17 +12,24 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+app.get("/", function (request, response) {
+	response.render('main');
+});
+
+app.get("/design", function(requset, response){
+	response.render('design');
+});
+
 app.get('/sendUserData', function(req, res){
 	var userId = req.query.userId;
 	var userPwd = req.query.password;
 	console.log(userId,userPwd);
+	res.json(1);
 });
 
-
-app.get("/", function (request, response) {
-	response.render('main');
-	/*var user_name = request.query.user_name;
-	response.end("Hello " + user_name + "!");*/
+app.get("/sayHello", function (request, response) {
+	var user_name = request.query.user_name;
+	response.end("Hello " + user_name + "!");
 });
 
 app.listen(port);
