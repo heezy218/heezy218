48f8fa4021a406ee963e2f78e79f9e26d96d0f58
heezy218 <heezy193@gmail.com>
2019-09-17 13:57:59 +0900

    test2

--- weather.js
+++ weather.js
@@ -4,9 +4,12 @@ var parser = require('xml2js');
 //terminal : npm i xml2js
 
 request('http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109', function(error, response, body){
-    parser.parseString(body, function (err, jsonData) {
+    /*parser.parseString(body, function (err, jsonData) {
         console.log(body);
         console.log(jsonData);
         callback(jsonData.rss.channel[0].item[0].description[0].header[0].wf[0]);
+    })*/
+    parser.parseString(body, function(err, jsonData){
+        console.log(jsonData.rss.channel[0].item[0].description[0].header[0].wf[0]);
     })
 });
