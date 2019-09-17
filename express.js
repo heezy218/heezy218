a58dcd3424d77808442a1acd567294511ffb6f09
heezy218 <heezy193@gmail.com>
2019-09-17 15:59:11 +0900

    fin

--- express.js
+++ express.js
@@ -1,4 +1,5 @@
 //npm install express
+//npm install ejs
 
 var express = require("express"),
 app = express();
@@ -10,11 +11,20 @@ app.engine('html', require('ejs').renderFile);
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/sendUserData', function(req, res){
+	var userId = req.query.userId;
+	var userPwd = req.query.password;
+	console.log(userId,userPwd);
+});
+
+
 app.get("/", function (request, response) {
 	response.render('main');
 	/*var user_name = request.query.user_name;
 	response.end("Hello " + user_name + "!");*/
 });
+
 app.listen(port);
 
 console.log("Listening on port ", port);
