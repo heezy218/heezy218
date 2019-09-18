c7f5220a8766ca6e2e0590bc910cf2159ca10074
heezy218 <heezy193@gmail.com>
2019-09-18 13:44:35 +0900

    2

--- express.js
+++ express.js
@@ -25,6 +25,28 @@ app.get("/signup", function(request, response){
 	response.render('signup');
 });
 
+app.get("/authResult", function(req, res){
+	var authResult = req.query.code;
+	console.log(authCode);
+	option = {
+		url : "https://testapi.open-platform.or.kr/oauth/2.0/token",
+		method :"POST",
+		headers : {
+		},
+		form : {
+			code : authCode,
+			client_id: "l7xx3d9f8f7f5978488da2fb458cb6e2411b",
+			client_secret: "a6998da5904e46518e21c8a92188a932",
+			redirect_uri: "http://localhost:3000/authResult",
+			grant_type : "authorization_code"
+		}
+	}
+	request(option, function(error,response, body){
+		console.log(body);
+		res.json(body);
+	})
+});
+
 app.get('/sendUserData', function(req, res){
 	var userId = req.query.userId;
 	var userPwd = req.query.password;
