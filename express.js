43cdd9e632d07f0f0888ccb93283d11c6624fde3
heezy218 <heezy193@gmail.com>
2019-09-19 10:31:58 +0900

    휴,,,

--- express.js
+++ express.js
@@ -1,26 +1,38 @@
-//
 //npm install express
 //npm install ejs
 
-var express = require("express"),
+var express = require("express");
+var jwt = require("jsonwebtoken");
 app = express();
-var request = require("request");
+var request = require('request');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'su218',   //
+  database : 'fintech'
+});
+connection.connect(); 
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (request, response) {
-	response.render('main');
+    response.render('main');
 });
 
-app.get("/design", function(request, response){
-	response.render('design');
+app.get("/design", function (request, response) {
+    response.render('design');
 });
+
 
 app.get("/signup", function(request, response){
 	response.render('signup');
@@ -36,8 +48,8 @@ app.get("/authResult", function(req, res){
 		},
 		form : {
 			code : authCode,
-			client_id: "l7xx3d9f8f7f5978488da2fb458cb6e2411b",
-			client_secret: "a6998da5904e46518e21c8a92188a932",
+			client_id: "l7xx3d9f8f7f5978488da2fb458cb6e2411b", //
+			client_secret: "a6998da5904e46518e21c8a92188a932", //
 			redirect_uri: "http://localhost:3000/authResult",
 			grant_type : "authorization_code"
 		}
@@ -57,10 +69,16 @@ app.get("/authResult", function(req, res){
 });
 
 app.get('/sendUserData', function(req, res){
-	var userId = req.query.userId;
-	var userPwd = req.query.password;
-	console.log(userId,userPwd);
-	res.json(1);
+    var userId = req.query.userId;
+    var userPwd = req.query.password;
+    console.log(userId, userPwd);
+    res.json(1);
+})
+
+app.get("/signup", function (request, response) {
+    console.log(request);
+    console.error(request.body);
+    response.render('signup');
 });
 
 app.get("/sayHello", function (request, response) {
@@ -68,33 +86,75 @@ app.get("/sayHello", function (request, response) {
 	response.end("Hello " + user_name + "!");
 });
 
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/balance', function(req, res){
+    
+})
+
+app.post('/login', function (req, res) {
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+    console.log(userEmail, userPassword);
+
+    var sql = "SELECT * FROM user WHERE user_id = ?";
+    connection.query(sql, [userEmail], function (error, results) {
+      if (error) throw error;  
+      else {
+
+        console.log(userPassword, results[0].user_password);
+        if(userPassword == results[0].user_password){
+            jwt.sign(
+                {
+                    userName : results[0].name,
+                    userId : results[0].user_id
+                },
+                "abcdefg123456",
+                {
+                    expiresIn : '1d',
+                    issuer : 'fintech.admin',
+                    subject : 'user.login.info'
+                },
+                function(err, token){
+                    console.log('로그인 성공', token)
+                    res.json(token)
+                }
+            )            
+        }
+        else {
+            res.json('등록정보가 없습니다');
+        }
+      }
+	});
+})
 
 app.post('/signup', function(req, res){
-	var userEmail = req.body.userEmail;
-	var userPassword = req.body.userPassword;
-	var accessToken = req.body.accessToken;
-	var refreshToken = req.body.refreshToken;
-	var useseqnum = req.body.useseqnum;
-
-	var sql = "INSERT INTO 'new_schema'.'user' {'user_id', 'user_password', 'phone', 'accessToken', 'refreshToekn', 'useseqnum'} " +
-	" VALUES (?,?,?,?,?,?,)";
-	RTCPeerConnection.query(sql,[userEmail,
-		userPassword,
-		"010",
-		accessToken,
-		refreshToken,
-		useseqnum ], function(err, result){
-		if(err){
-			console.error(err);
-			throw err;
-		}
-		else {
-
-		}
-	})
+    var userEmail = req.body.userEmail;
+    var userPassword = req.body.userPassword;
+    var accessToken = req.body.accessToken;
+    var refreshToken = req.body.refreshToken;
+    var useseqnum = req.body.useseqnum;
+    console.log(userEmail, userPassword, accessToken, refreshToken, useseqnum);
+    var sql = "INSERT INTO `fintech`.`user` " +
+    "(`user_id`, `user_password`, `phone`, `accessToken`, `refreshToken`, `userseqnum`)"+
+    " VALUES (?,?,?,?,?,?)";
+    connection.query(sql,[userEmail,
+        userPassword,
+        "010",
+        accessToken ,
+        refreshToken,
+        useseqnum ],function(err, result){
+        if(err){
+            console.error(err);
+            throw err;
+        }
+        else {
+            res.json(1);
+        }
+    })
 })
 
 app.listen(port);
-
 console.log("Listening on port ", port);
-
--- request_test.js
+++ request_test.js
@@ -4,7 +4,7 @@
 
 var request = require("request");
 
-request('http://www.naver.com', function(error, response, body){
+request('https://testapi.open-platform.or.kr/user/me?user_seq_no=1100035493', function(error, response, body){
     console.log('error:', error);
     console.log('statusCode:', response && response.statusCode);
     console.log('body:', body); // Print the HTML for the Google homepage
